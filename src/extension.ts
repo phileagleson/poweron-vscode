2da1457efc285ff7dec90bd44586e2b22519c898
Phil Eagleson <peagleson@symitar.com>
2023-01-04 16:19:56 -0500

    updated poweron language server to fix completion bug

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,10 @@ import * as path from 'path'
 import { CodeAction, CodeActionKind, commands, ExtensionContext, window } from 'vscode'
 
 import {
- LanguageClient,
- LanguageClientOptions,
- ServerOptions,
- TransportKind
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
 } from 'vscode-languageclient/node'
 
 let client: LanguageClient
@@ -18,61 +18,63 @@ export const DEFINETITLE = `Add Variable to DEFINE division`
 
 
 export function activate(context: ExtensionContext) {
- const serverModule = context.asAbsolutePath(
-  process.env['POWERON_SERVER_PATH'] ||
-  path.join('node_modules', 'poweron-language-server', 'out', 'main.js')
- )
-
- const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
-
- const serverOptions: ServerOptions = {
-  run: {
-   module: serverModule,
-   transport: TransportKind.ipc,
-  },
-  debug: {
-   module: serverModule,
-   transport: TransportKind.ipc,
-   options: debugOptions
-  }
- }
-
- const clientOptions: LanguageClientOptions = {
-  documentSelector: [{ scheme: 'file', language: 'poweron' }],
-  progressOnInitialization: true,
- }
-
- client = new LanguageClient(
-  'poweronlsp',
-  'PowerOn Language Server',
-  serverOptions,
-  clientOptions
- )
-
- client.onReady().then(() => {
-  context.subscriptions.push(
-   commands.registerCommand('poweronlsp.showDataTypeNotification', handleNotification)
+  const serverModule = context.asAbsolutePath(
+    process.env['POWERON_SERVER_PATH'] ||
+    path.join('node_modules', 'poweron-language-server', 'out', 'main.js')
   )
- })
 
- client.start()
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
+
+  const serverOptions: ServerOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions
+    }
+  }
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'poweron' }],
+    progressOnInitialization: true,
+  }
+
+  client = new LanguageClient(
+    'poweronlsp',
+    'PowerOn Language Server',
+    serverOptions,
+    clientOptions
+  )
+
+  client.onReady().then(() => {
+    context.subscriptions.push(
+      commands.registerCommand('poweronlsp.showDataTypeNotification', handleNotification)
+    )
+  })
+
+  client.start()
 }
 
 export function deactivate(): Thenable<void> | undefined {
- if (!client) {
-  return undefined
- }
- return client.stop()
+  if (!client) {
+    return undefined
+  }
+  return client.stop()
 }
 
 async function handleNotification(arg1: any, arg2: any, arg3: any) {
- const dataType = await window.showQuickPick(['CHARACTER', 'CODE', 'DATE','FLOAT','MONEY','NUMBER','RATE'])
- const {uri, varName } = arg1
- client.sendRequest('workspace/executeCommand', { command: `lsp.addVarToDefine`, arguments: [{
-    uri,
-    varName,
-    dataType
- }]})
- 
- 
+  const dataType = await window.showQuickPick(['CHARACTER', 'CODE', 'DATE', 'FLOAT', 'MONEY', 'NUMBER', 'RATE'])
+  const { uri, varName } = arg1
+  client.sendRequest('workspace/executeCommand', {
+    command: `lsp.addVarToDefine`, arguments: [{
+      uri,
+      varName,
+      dataType
+    }]
+  })
+
+
 }
