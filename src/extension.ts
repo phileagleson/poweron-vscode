ab3b6649d50e52451d5164c3948567333520b7c8
Phil Eagleson <peagleson@symitar.com>
2023-10-11 17:19:49 -0400

    New pols branch

--- src/extension.ts
+++ src/extension.ts
@@ -1,80 +1,131 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-import * as path from 'path'
-import { CodeAction, CodeActionKind, commands, ExtensionContext, window } from 'vscode'
+import * as path from "path";
+import {
+  commands,
+  ExtensionContext,
+  window,
+  workspace,
+} from "vscode";
 
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
-  TransportKind
-} from 'vscode-languageclient/node'
+  TransportKind,
+} from "vscode-languageclient/node";
 
-let client: LanguageClient
-export const DEFINETITLE = `Add Variable to DEFINE division`
-
+let client: LanguageClient;
+export const DEFINETITLE = `Add Variable to DEFINE division`;
 
 export function activate(context: ExtensionContext) {
-  const serverModule = context.asAbsolutePath(
-    process.env['POWERON_SERVER_PATH'] ||
-    path.join('node_modules', 'poweron-language-server', 'out', 'main.js')
-  )
-
-  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
+  const serverModule = context.asAbsolutePath(path.join("pols64.exe"));
 
   const serverOptions: ServerOptions = {
     run: {
-      module: serverModule,
+      command: serverModule,
       transport: TransportKind.ipc,
     },
     debug: {
-      module: serverModule,
+      command: serverModule,
       transport: TransportKind.ipc,
-      options: debugOptions
-    }
-  }
+    },
+};
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: 'file', language: 'poweron' }],
+    documentSelector: [{ scheme: "file", language: "poweron" }],
     progressOnInitialization: true,
-  }
+  };
 
   client = new LanguageClient(
-    'poweronlsp',
-    'PowerOn Language Server',
+    "poweronlsp",
+    "PowerOn Language Server",
     serverOptions,
     clientOptions
-  )
+  );
 
   client.onReady().then(() => {
-    context.subscriptions.push(
-      commands.registerCommand('poweronlsp.showDataTypeNotification', handleNotification)
+    const customCommands = []
+    customCommands.push(
+      commands.registerCommand(
+        "poweron.showDataTypeNotification",
+        handleNotification
+      ),
     )
-  })
 
-  client.start()
+    customCommands.push(
+      commands.registerCommand(
+        "poweron.validatePoweron",
+        validatePoweron
+      )
+    )
+
+    const configHandler = client.onNotification("workspace/configuration",() =>{
+      const config = workspace.getConfiguration()
+      let syms =  config.get("poweron.symConfigurations") as Array<any>
+      client.sendNotification("workspace/didChangeConfiguration",{
+        symConfigurations: syms,
+      })
+    })
+  
+    context.subscriptions.push(...customCommands,configHandler);
+
+  });
+
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
   if (!client) {
-    return undefined
+    return undefined;
   }
-  return client.stop()
+  return client.stop();
+}
+
+async function validatePoweron() {
+  const config = workspace.getConfiguration()
+  let syms =  config.get("poweron.symConfigurations") as Array<any>
+
+  console.log({syms})
+
+  const symConfigName = await window.showQuickPick(syms.map(sym =>{
+    return sym.name
+  })) 
+
+  console.log({uri:window.activeTextEditor.document.uri.toString()})
+
+  if (symConfigName) {
+    client.sendNotification("workspace/executeCommand", {
+      command: "lsp.validatePoweron",
+      arguments: [
+        {
+          symConfigName,
+          uri: window.activeTextEditor.document.uri.toString()
+        }
+      ]
+
+    })
+  }
+
 }
 
 async function handleNotification(arg1: any, arg2: any, arg3: any) {
-  const dataType = await window.showQuickPick(['CHARACTER', 'CODE', 'DATE', 'FLOAT', 'MONEY', 'NUMBER', 'RATE'])
-  const { uri, varName } = arg1
-  client.sendRequest('workspace/executeCommand', {
-    command: `lsp.addVarToDefine`, arguments: [{
-      uri,
-      varName,
-      dataType
-    }]
-  })
-
-
+  const dataType = await window.showQuickPick([
+    "CHARACTER",
+    "CODE",
+    "DATE",
+    "FLOAT",
+    "MONEY",
+    "NUMBER",
+    "RATE",
+  ]);
+  const { uri, varName } = arg1;
+  await client.sendRequest("workspace/executeCommand", {
+    command: `lsp.addVarToDefine`,
+    arguments: [
+      {
+        uri,
+        varName,
+        dataType,
+      },
+    ],
+  });
 }
